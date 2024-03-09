3ee1868d9817e50303fa7c853811747748f065c3
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2024-03-09 17:07:11 +0100

    Add more tests

---
+++ sekura/archive/tests/test_views.py
@@ -0,0 +1,18 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class ListTest(TestCase):
+    def setUp(self):
+        login = "foo"
+        pwd = "foo"
+        user = User.objects.create_user(username=login, password=pwd)
+        self.client = Client()
+        self.client.login(username=login, password=pwd)
+
+    def test_no_archives(self):
+        r = self.client.get(reverse("archive:index"))
+        assert r.response_code == 200
+
+
