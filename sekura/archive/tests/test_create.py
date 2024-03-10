cf9d4fd1d3b4a77cbd533538def162c1bd56840d
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2024-03-10 19:02:53 +0100

    fix pre-commit

--- sekura/archive/tests/test_create.py
+++ sekura/archive/tests/test_create.py
@@ -1,40 +1,55 @@
+import tempfile
+from io import StringIO
+
+from bs4 import BeautifulSoup
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
-from bs4 import BeautifulSoup
-from io import StringIO
-from django.conf import settings
-import tempfile
 
 from .. import models
+
 
 class CreateTest(TestCase):
     def setUp(self):
         login = "foo"
         pwd = "foo"
-        user = User.objects.create_user(username=login, password=pwd)
+        User.objects.create_user(username=login, password=pwd)
         self.client = Client()
         self.client.login(username=login, password=pwd)
         settings.MEDIA_ROOT = tempfile.mkdtemp()
 
-
     def test_get_returns_form(self):
+        # GIVEN
+
+        # WHEN
         r = self.client.get(reverse("archive:create"))
 
+        # THEN
         soup = BeautifulSoup(r.content, features="html.parser")
 
         assert soup.form.get("action") == "/archive/create/"
-        assert soup.form.get("method")== "post"
+        assert soup.form.get("method") == "post"
         assert soup.form.get("enctype") == "multipart/form-data"
 
         csrf = soup.form.find("input", {"name": "csrfmiddlewaretoken"})
         assert csrf.get("type") == "hidden"
 
     def test_post_empty_returns_invalid_form(self):
+        # GIVEN
+
+        # WHEN
         r = self.client.post(reverse("archive:create"))
+
+        # THEN
         assert b"This field is required." in r.content
 
     def test_post_valid(self):
+        # GIVEN
         fd = StringIO("This is a test")
-        r = self.client.post(reverse("archive:create"), {"name": "test", "content": fd})
+
+        # WHEN
+        self.client.post(reverse("archive:create"), {"name": "test", "content": fd})
+
+        # THEN
         assert models.Archive.objects.count() == 1
--- sekura/archive/tests/test_views.py
+++ sekura/archive/tests/test_views.py
@@ -1,17 +1,22 @@
+from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
 
 
 class ListTest(TestCase):
     def setUp(self):
         login = "foo"
         pwd = "foo"
-        user = User.objects.create_user(username=login, password=pwd)
+        User.objects.create_user(username=login, password=pwd)
         self.client = Client()
         self.client.login(username=login, password=pwd)
 
     def test_no_archives(self):
+        # GIVEN
+
+        # WHEN
         r = self.client.get(reverse("archive:index"))
+
+        # THEN
         assert r.status_code == 200
         assert b"No archives are available" in r.content
